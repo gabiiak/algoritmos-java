a0ae7b11a60de96830ea2d95aa20c6bbc743f2c2
gabii.ak <elpollokawaiicomecoco@gmail.com>
2025-10-23 13:34:59 -0300

    ARBOLES Y MÁS ARBOLES!!!

--- src/main/java/arbolesbinarios/Arbol.java
+++ src/main/java/arbolesbinarios/Arbol.java
@@ -6,7 +6,7 @@ public class Arbol {
         ArbolBinario arbolito = new ArbolBinario();
         
         teclado = new Scanner(System.in);
-        System.out.print("holi. insertá la cantidad de nodos que querés para el arbol y el valor no te preocupes, se genera aleatoriamente: ");
+        System.out.print("Holi. Insertá la cantidad de nodos que querés para el arbol y el valor no te preocupes, se genera aleatoriamente: ");
         int cantNodos = teclado.nextInt();
         int[] vector = arbolito.numAleatorio(cantNodos);
         //int[] vector = {30, 60 , 50, 29, 100, 213}; para comprobar que el método existevalor funciona
@@ -15,14 +15,31 @@ public class Arbol {
             arbolito.insertar(vector[i]);
         }
 
-        System.out.println("\nlos nodos fueron insertados correctamente.");
+        System.out.println("\nLos nodos fueron insertados correctamente.");
         
         arbolito.menorValorDerecha();
         arbolito.mayorValorIzquierda();
+        
+        int nivel = arbolito.existeValorEnNivel(29);
+        if (nivel != -1) System.out.println("El valor se encuentra en el nivel " + nivel);
+        else System.out.println("El num 29 no existe en el arbol");
+        /*CORRECCIÓN POR TARS 
         arbolito.existeValorEnNivel(29); //al ser valores aleatorios, el 29 puede existir... o no...
-        System.out.println("cantidad de hojas del arbol: " + arbolito.contarHojas(arbolito.raiz));
-        System.out.println("arbol ordenado por entreorden");
+        */
+        
+        System.out.println("Cantidad de hojas del arbol: " + arbolito.contarHojas(arbolito.raiz));
+        System.out.println("Cantidad de ramas del arbol: " + arbolito.contarNodosInternos(arbolito.raiz));
+        System.out.println("Altura del arbol: " + arbolito.alturaArbol(arbolito.raiz));
+        
+        System.out.println("\nArbol ordenado por preorden");
+        arbolito.preOrden(arbolito.raiz);
+
+        System.out.println("\nArbol ordenado por entreorden");
         arbolito.entreOrden(arbolito.raiz);
+
+        System.out.println("\nArbol ordenado por postorden");
+        arbolito.postOrden(arbolito.raiz);
+        
         teclado.close();
     }
     
--- src/main/java/arbolesbinarios/ArbolBinario.java
+++ src/main/java/arbolesbinarios/ArbolBinario.java
@@ -39,41 +39,43 @@ class ArbolBinario{
 
         //MENOR VALOR EN EL SUB ARBOL DERECHO
         public void menorValorDerecha(){
-            if (raiz != null){
-                Nodo reco = raiz.derecha;
-                if (raiz == null || raiz.derecha == null) {
-                    System.out.println("No hay subárbol derecho.");
-                    return;
-                }
-                while (reco.izquierda != null){
-                    reco = reco.izquierda;
-                }
-                System.out.println("el valor más chico de la derecha es: " + reco.dato);
+            Nodo reco = raiz.derecha;
+            if (raiz == null || raiz.derecha == null) {
+                System.out.println("No hay subárbol derecho.");
+                return;
             }
+            while (reco.izquierda != null){
+                reco = reco.izquierda;
+            }
+            System.out.println("el valor más chico de la derecha es: " + reco.dato);        
         }
 
         //MAYOR VALOR EN EL SUB ARBOL IZQUIERDO
         public void mayorValorIzquierda(){
-            if (raiz != null){
-                Nodo reco = raiz.izquierda;
-                if (raiz == null || raiz.izquierda == null) {
-                    System.out.println("No hay subárbol derecho.");
-                    return;
-                }
-                while(reco.derecha != null){
-                    reco = reco.derecha;
-                }
-                System.out.println("el valor más grande de la izquierda es: " + reco.dato);
+            Nodo reco = raiz.izquierda;
+            if (raiz == null || raiz.izquierda == null) {
+                System.out.println("No hay subárbol izquierdo.");
+                return;
             }
+            while(reco.derecha != null){
+                reco = reco.derecha;
+            }
+            System.out.println("el valor más grande de la izquierda es: " + reco.dato);
         }
 
         //SI EL VALOR EXISTE EN EL NIVEL
-        public void existeValorEnNivel (int dato){
-            int nivel = 1;
-            existeValor (raiz, nivel, dato);
+        //* CORRECCIÓN POR TARS
+        public int existeValorEnNivel (int dato){
+            return existeValor(raiz, 1, dato);
         }
-        private void existeValor (Nodo reco, int nivel, int dato){
-            if (reco != null){
+        private int existeValor (Nodo reco, int nivel, int dato){
+            if (reco == null) return -1;
+            if (reco.dato == dato) return nivel;
+            int nivelizq = existeValor(reco.izquierda, nivel + 1, dato);
+            if (nivelizq != -1) return nivelizq;
+            return existeValor(reco.derecha, nivel + 1, dato);
+
+            /*if (reco != null){
                 existeValor(reco.izquierda, nivel + 1 , dato);
                 if (reco.dato == dato){
                     System.out.println("el valor " + dato + " se encuentra en el nivel " + nivel);
@@ -81,13 +83,29 @@ class ArbolBinario{
                 existeValor(reco.derecha, nivel + 1, dato);
 
             }//si no existe el nodo (29), no se ejecuta
+            */
         }
         
-        //ALTURA DEL ARBOL
+        //CANTIDAD DE NODOS HOJAS
         public int contarHojas (Nodo reco){
             if (reco == null) return 0;
             if (reco.izquierda == null && reco.derecha == null ) return 1;
             return contarHojas(reco.izquierda) + contarHojas(reco.derecha);
+        }
+
+        //CANTIDAD DE NODOS RAMA (internos)
+        public int contarNodosInternos(Nodo reco){
+            if (reco == null || reco.izquierda == null && reco.derecha == null) return 0;
+            return 1 + contarNodosInternos(reco.izquierda) + contarNodosInternos(reco.derecha);
+        }
+
+        //ALTURA DEL ARBOL
+        public int alturaArbol(Nodo reco){
+            if (reco == null) return 0;
+            int altizq = alturaArbol(reco.izquierda);
+            int altder = alturaArbol(reco.derecha);
+            if (altizq > altder) return altizq + 1;
+            else return altder + 1;
         }
 
         //ENTREORDEN DEL ARBOL
@@ -100,4 +118,23 @@ class ArbolBinario{
             }
         }
 
+        //PREORDEN DEL ARBOL
+        // RAIZ -> IZQUIERDA -> DERECHA
+        public void preOrden(Nodo reco){
+            if (reco != null){
+                System.out.print(reco.dato  + " - ");
+                preOrden(reco.izquierda);
+                preOrden(reco.derecha);
+            }
+        }
+
+        //POSTORDEN DEL ARBOL
+        //IZQUIERDA -> DERECHA -> RAIZ
+        public void postOrden(Nodo reco){
+            if (reco != null){
+                postOrden(reco.izquierda);
+                postOrden(reco.derecha);
+                System.out.print(reco.dato + " - ");
+            }
+        }
     }
