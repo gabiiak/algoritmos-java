1b8026c664dee9c4f541a114668a41e5b6c8718f
gabii.ak <elpollokawaiicomecoco@gmail.com>
2025-10-23 04:23:16 -0300

    cambios a la estructura de código

---
+++ src/main/java/arbolesbinarios/Nodo.java
@@ -0,0 +1,10 @@
+package arbolesbinarios;
+
+class Nodo{
+    int dato;
+    Nodo izquierda,derecha;
+    public Nodo(int dato){
+        this.dato = dato;
+        izquierda = derecha = null;
+    }
+}
